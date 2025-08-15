6ddb25bb4b573efdb1d8981d4c648b34db37aa1e
VigneshKri <20ise0047@vvce.ac.in>
2025-08-15 23:35:54 +0530

    fix: Some changes

--- sample.java
+++ sample.java
@@ -2,6 +2,6 @@ public class sample {
     public static void main(String[] args) {
         System.out.println("Jenkins check");
         System.out.println("Jenkins edit test");
-        System.out.println("Auto Trigger check");
+        System.out.println("Auto Trigger check ");
     }
 }
