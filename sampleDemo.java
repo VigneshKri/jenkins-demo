b976f14359dc39fac41cc346fe1fbc0b26d4f35b
VigneshKri <20ise0047@vvce.ac.in>
2025-08-16 11:50:38 +0530

    feat: New change

--- sampleDemo.java
+++ sampleDemo.java
@@ -1,4 +1,4 @@
-public class sample {
+public class sampleDemo {
     public static void main(String[] args) {
         System.out.println("Jenkins check");
         System.out.println("Jenkins edit test");
